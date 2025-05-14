fe51830fa3f2a7504fea4b2855bf519577ea993a
Rodrigo Ramalho <rodrigo51685776@aluno.ms.senac.br>
2025-05-13 21:52:57 -0400

    Starting project

---
+++ components/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="container-navbar">
+        <ul class="list-navbar">
+            <li class="item-navbar"><a href="#" class="anchor-item-navbar">Home</a></li>
+            <li class="item-navbar"><a href="#" class="anchor-item-navbar">Login</a></li>
+            <li class="item-navbar"><a href="#" class="anchor-item-navbar">Perfil</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once './components/head.php'; ?>
+
+<body>
+    <?php require_once './components/navbar.php'; ?>
+    
+</body>
+</html>
